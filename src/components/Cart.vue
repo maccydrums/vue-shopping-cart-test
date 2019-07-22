ab0cbd858a30072cfb5426e6279665a6aa600c21
maccydrums <marcusprivat@live.com>
2019-07-22 13:30:56 +0200

    done

---
+++ src/components/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+    <ul class="list-group">
+      <li class="list-group-item" :key="item.id" v-for="item in items">
+        {{ item.title }} <br/>$ {{ item.price }} + shipping
+        <button
+          @click="$emit('remove-from-cart', item)"
+          class="btn badge-danger float-right"
+        >Remove from cart</button>
+      </li>
+    </ul>
+    <div class="p-3 mt-5">
+      <h4 class="text-center">Total: $ {{ total }}</h4>
+    </div>
+    <button :disabled="items.length === 0" @click="$emit('pay')" class="btn btn-info form-control center">Pay Now</button>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+  computed: {
+    total() {
+      return this.items.reduce((acc, item) => acc + Number(item.price), 0)
+    }
+  }
+}
+</script>
---
+++ src/components/Product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card my-5">
+    <div class="card-body">
+      <img v-bind:src="product.image" alt="" style="width: 100%">
+      <h5 class="text-center card-title">{{ product.title }}</h5>
+      <p class="text-center text-muted card-text display-4">$ {{ Number(product.price).toFixed() }}</p>
+      <button :disabled="isInCart" @click="$emit('add-to-cart', product)" class="btn btn-primary form-control">
+        {{ isInCart ? 'Added To Cart' : 'Add To Cart' }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product', 'isInCart']
+}
+</script>
